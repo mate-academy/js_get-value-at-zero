58a2838c5513c39cf9cffcbf1767d9aeced561d9
VlaDinter <vladyslavdinter@gmail.com>
2020-05-31 02:19:20 +0300

    Solution

--- src/getValueAtZero.js
+++ src/getValueAtZero.js
@@ -1,22 +1,7 @@
 'use strict';
 
-/**
- * Implement getValueAtZero function:
- *
- * it takes another function 'initialFunction' and returns it's value with 0 as
- * argument
- *
- * const initialFunction = function(x) {
- *     return x + 5
- * };
- * getValueAtZero(initialFunction) === 5;
- *
- * @param {function} initialFunction
- *
- * @return {function}
- */
 function getValueAtZero(initialFunction) {
-  // write code here
+  return initialFunction(0);
 }
 
 module.exports = getValueAtZero;
